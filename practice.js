f68a888e13491b5247b747a1e2156ddf7b711f5b
ms.moradi23 <ms.moradi23@yahoo.com>
2022-08-18 09:08:10 +0430

    completion of the 2th homework

---
+++ practice.js
@@ -0,0 +1,45 @@
+// 1 //...................
+// console.log( Number("234a")); // output : NaN
+// console.log('1'+ 2 +2); // output : 122
+
+// let appels = "2";
+// let oranges = "3";
+// console.log( +appels + +oranges); // output : 5
+
+// let counter = 1;
+// console.log( 2 * ++counter); // output : 4
+
+// console.log(undefined == null); // output : true
+
+// console.log (undefined === null);  // output : false
+
+// console.log(null == "\0n\n");  // output : false
+
+// console.log(null === +"\n0\n");  // output : false
+
+// 2 //...................
+
+// let a=Number(prompt("Enter first number"));
+// let b=Number(prompt("Enter second number"));
+// let c=Number(prompt("Enter third number"));
+// (a<b+c && b<a+c && c<a+b) ? console.log("true"): console.log("no");
+
+// 3 //....................
+// let number = Number(prompt("Enter a number"));
+// let count = 0;
+// while(number>0){
+//     number=Math.round(number/10);
+//     count +=1;
+// }
+// console.log(count);
+
+// 4 //....................
+// let result;
+// let f=Number(prompt("Enter a number"));
+// let e=Number(prompt("Enter the seconde number"));
+// (f<e)? result=f : result=e ;
+// for (let i=1 ; i<=f ; i++){
+//     if(f%i==0 && e%i==0){
+//         console.log(i);
+//     };
+// };
